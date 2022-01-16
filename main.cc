95b6e44b16cbe38775701bd5fbd189e126926b12
Etienne Sharpin <etienne.sharpin@epita.fr>
2022-01-16 22:08:13 +0100

    ADD: first working version (without temp)

--- main.cc
+++ main.cc
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <random>
 
 struct tile {
     char up;
@@ -35,42 +37,43 @@ inline const int get_x_dim(const std::vector<tile>& s)
 inline const unsigned int num_wrong_links(const std::vector<tile>& s, const unsigned int i)
 {
     static const int x_dim = get_x_dim(s);
-    static const int last_row_first_index = x_dim * (x_dim - 1);
 
     unsigned int res = 0;
 
     // TODO speedup by not using if and adding a boolean directly
 
+    /* We do not use those 2 since we iterate on every tiles, links would be counted 2 times
+    static const int last_row_first_index = x_dim * (x_dim - 1);
+
     // If not on last row
     if (!(i >= last_row_first_index))
     {
         // Get num_wrong_links with lower tile
-        res += s[i].down == s[i + x_dim].up;
+        res += s[i].down != s[i + x_dim].up;
     }
 
     // If not on last column
     if (!(i % x_dim == x_dim - 1))
     {
         // Get num_wrong_links with right tile
-        res += s[i].right == s[i + 1].left;
+        res += s[i].right != s[i + 1].left;
     }
-
-    /* We do not use those 2 since we iterate on every tiles, links would be counted 2 times
+    */
+    
     // If not en first column
     if (!(i % x_dim == 0))
     {
         // Get num_wrong_links with left tile
-        res += s[i].left == s[i - 1].right;
+        res += s[i].left != s[i - 1].right;
     }
-
 
     // If not on first row
     if (!(i < x_dim))
     {
         // Get num_wrong_links with upper tile
-        res += s[i].up == s[i - x_dim].down;
+        res += s[i].up != s[i - x_dim].down;
     }
-    */
+    
     return res;
 }
 
@@ -87,16 +90,56 @@ unsigned int E(const std::vector<tile>& s)
     return energy;
 }
 
-void find_best_tiles_setup(std::vector<tile>& tiles, std::vector<int>& moveableTilesIndexes)
+const std::vector<tile> voisin(const std::vector<tile>& s0, const std::vector<int>& moveableTilesIndexes)
 {
-    std::vector<tile> s = tiles;
-    std::vector<tile> g = tiles;
+    std::vector<int> out;
+    std::sample(
+        moveableTilesIndexes.begin(),
+        moveableTilesIndexes.end(),
+        std::back_inserter(out),
+        2,
+        std::mt19937{std::random_device{}()}
+    );
+
+    std::vector<tile> res = s0;
+
+    std::swap(res[out[0]], res[out[1]]);
+    
+    return res;
+}
+
+std::vector<tile> find_best_tiles_setup(std::vector<tile>& s0, const std::vector<int>& moveableTilesIndexes)
+{
+    std::vector<tile> s = s0;
+    std::vector<tile> g = s0;
 
     unsigned int e = E(s);
     unsigned int m = e;
     unsigned int k = 0;
 
-    
+
+    while (true)
+    {
+        const std::vector<tile> sn = voisin(s, moveableTilesIndexes);
+        const unsigned int en = E(sn);
+
+        if (en < e || (float) rand()/RAND_MAX < 0)//P(en -e, temp(k/kmax)))
+        {
+            s = sn;
+            e = en;
+        }
+        
+        if (e < m)
+        {
+            g = s;
+            m = e;
+
+            if (e == 0)
+                return g;
+        }
+
+        ++k;
+    }
 }
 
 int main(int argc, char** argv)
@@ -132,7 +175,7 @@ int main(int argc, char** argv)
     }
 
     // Apply Simulated Annealing
-    find_best_tiles_setup(tiles, moveableTilesIndexes);
+    tiles = find_best_tiles_setup(tiles, moveableTilesIndexes);
 
     // Flush new tiles to output file
     std::vector<tile>::iterator it = tiles.begin();
